920b5b8f5552b001d3d311282583b9ab9eb765d3
tuilanomoka <tuilanomoka@gmail.com>
2025-11-24 19:38:52 +0700

    Update tí xíu

--- app.py
+++ app.py
@@ -798,4 +798,4 @@ def api_admin_set_points():
 
 if __name__ == '__main__':
     Db.create_accounts_table()
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
